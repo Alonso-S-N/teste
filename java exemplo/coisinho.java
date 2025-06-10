a134676a2f670bf3c2fb5f5f387b7a9a14258868
AlonsoS <ghoul6413@gmail.com>
2025-06-09 21:24:53 -0300

    coisin

---
+++ java exemplo/coisinho.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[]args){
+
+        Video v [] = new Video[5];
+
+        v[0] = new Video("aula 1 poo",150,true,500,20);
+        System.out.println(v[0].toString());
+
+        pessoa p = new pessoa(0,20,"Alonso","M");
+        System.out.println(p.toString());
+
+        Gafanhoto g [] = new Gafanhoto[2];
+        g[0] = new Gafanhoto(0,15,"Alonso","M","jubileuDoGrau123",120 );
+        System.out.println(g[0].toString());
+
+        Visualização J = new Visualização(g[0],v[0]);
+        J.Avaliar(35.0f);
+        System.out.println(J.toString());
+    }
+
+}
